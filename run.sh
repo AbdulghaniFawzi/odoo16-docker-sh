700bca001d6c81924df3fbc05d5fa3eb3097d627
elblasy33 <44347411+elblasy33@users.noreply.github.com>
2022-12-23 20:53:08 +0200

    Update run.sh
    fix port

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
-sed -i 's/10015/'$PORT'/g' $DESTINATION/docker-compose.yml
-sed -i 's/20015/'$CHAT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Odoo
 docker-compose -f $DESTINATION/docker-compose.yml up -d
 
